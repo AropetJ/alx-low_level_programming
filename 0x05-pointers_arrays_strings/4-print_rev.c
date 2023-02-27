4a40227732a6e55e8ca57ed55a51c371c4c4fefc
AropetJ <aropetjoel32@gmail.com>
2023-02-27 21:22:45 +0300

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - imprime en reverse
+ *
+ * @s: string
+ *
+ * return: 0
+ */
+
+void print_rev(char *s)
+{
+	int l = 0;
+	int m;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	s--;
+	for (m = l; m > 0; m--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
