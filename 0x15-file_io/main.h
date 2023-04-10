22b2797f6797abdbe2ec25299892242ed6bbb2ea
AropetJ <aropetjoel32@gmail.com>
2023-04-10 15:34:05 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,8 +3,7 @@
 
 #include <fcntl.h>
 #include <sys/types.h>
-#include <unistd.h>
-#include <unistd>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
