fd246473d36bed38b9bf8d1e029520a13404f311
AropetJ <aropetjoel32@gmail.com>
2023-04-10 14:59:55 -0700

    indiannes

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks if a computer is big or little endian
+ *
+ * Return: 0 for big, 1 for little
+ *
+ */
+
+int get_endianness(void)
+{
+	unsigned int j = 1;
+	char *ch = (char *) &j;
+
+	return (*ch);
+}
