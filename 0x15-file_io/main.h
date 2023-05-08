c1851cfcbc9eb855d5379f444416afa7aa99dec3
AropetJ <aropetjoel32@gmail.com>
2023-05-08 16:51:46 +0200

    All files

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
