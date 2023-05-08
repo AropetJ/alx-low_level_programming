49419d5d8e299a2667b0fae307af553621cc75d7
AropetJ <aropetjoel32@gmail.com>
2023-05-08 12:11:34 -0700

    creates files 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,8 +32,8 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	wr = write(fd, text_content, len);
-	if (w == -1)
+	wr = write(fd, text_content, i);
+	if (wr == -1)
 		return (-1);
 
 	close(fd);
