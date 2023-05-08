4684cb770e6f9fc0bba4eee54c0e2587cf90f53e
AropetJ <aropetjoel32@gmail.com>
2023-05-08 13:24:23 -0700

    read text 9

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,18 +18,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t wr;
 	ssize_t r;
 
-	fd = open(filename, O_RDONLY);
+	if (filename == NULL)
 	{
-		if (fd == -1)
-			return (0);
+		return (0);
 	}
-
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		return (0);
 	}
 
 	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+	{
+		return (0);
+	}
+
 	r = read(fd, buff, letters);
 	wr = write(STDOUT_FILENO, buff, r);
 	if (wr == -1)
