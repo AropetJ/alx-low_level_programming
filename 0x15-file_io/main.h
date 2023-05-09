9211d4fb355b986516d85705c6b536abd6400928
AropetJ <aropetjoel32@gmail.com>
2023-05-08 17:25:56 -0700

    header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -24,5 +24,8 @@
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
+char *make_buff(char *file);
+void close_fd(int fd);
+int _putchar(char c);
 
 #endif
