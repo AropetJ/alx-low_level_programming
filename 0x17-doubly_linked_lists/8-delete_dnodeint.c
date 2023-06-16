0b774517fedab090e560bbded653a1b9a79174af
AropetJ <aropetjoel32@gmail.com>
2023-06-17 02:59:53 +0300

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,42 +1,47 @@
 #include "lists.h"
 
 /**
- *delete_dnodeint_at_index - function that delete a node.
- *@head: the head of the node.
- *@index: the index to delete.
- *Return: 1 successfull, -1 error.
+ * delete_dnodeint_at_index - function that delete a node.
+ *
+ * @head: the head of the node.
+ * @index: the index to delete.
+ *
+ * Return: 1 successfull, -1 error.
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-unsigned int i = 0;
-dlistint_t *tmp, *prevn;
-if (*head == NULL)
-return (-1);
-if (index == 0)
-{
-tmp = *head;
-*head = (*head)->next;
-free(tmp);
-return (1);
-}
-else
-{
-tmp = (*head)->next;
-prevn = *head;
-while (tmp)
-{
-if (index == i)
-{
-prevn->next = tmp->next;
-(tmp->next)->prev = prevn;
-free(tmp);
-return (1);
-}
-tmp = tmp->next;
-prevn = prevn->next;
-i++;
-}
-}
-return (-1);
+	dlistint_t *temp, *prevn;
+	unsigned int j = 0;
+	
+	if (*head == NULL)
+		return (-1);
+	
+	if (index == 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+	else
+	{
+		temp = (*head)->next;
+		prevn = *head;
+		while (temp)
+		{
+			if (index == j)
+			{
+				prevn->next = temp->next;
+				(temp->next)->prev = prevn;
+				free(temp);
+				return (1);
+			}
+			temp = temp->next;
+			prevn = prevn->next;
+			j++;
+		}
+	}
+	
+	return (-1);
 }
