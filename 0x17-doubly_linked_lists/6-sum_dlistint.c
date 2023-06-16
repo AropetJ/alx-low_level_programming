11764e002cc5c0ec81eea7eb15bb19ed1e988c43
AropetJ <aropetjoel32@gmail.com>
2023-06-17 02:35:26 +0300

    6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,22 +1,25 @@
 #include "lists.h"
 
 /**
- *sum_dlistint - the sum of all integers
- *@head: the head of list.
- *Return: the sum.
+ * sum_dlistint - the sum of all integers
+ *
+ * @head: the head of list.
+ *
+ * Return: the sum.
  */
 
 int sum_dlistint(dlistint_t *head)
 {
-int sum = 0;
+	int sum = 0;
 
-dlistint_t *tmp = head;
-if (!head)
-return (0);
-while (tmp)
-{
-sum += tmp->n;
-tmp = tmp->next;
-}
-return (sum);
+	dlistint_t *tmp = head;
+	
+	if (!head)
+		return (0);
+	while (tmp)
+	{
+		sum += tmp->n;
+		tmp = tmp->next;
+	}
+	return (sum);
 }
