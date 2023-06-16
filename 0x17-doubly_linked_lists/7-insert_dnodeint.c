0fa345792fb8794279d1d4e68731f3278c704e26
AropetJ <aropetjoel32@gmail.com>
2023-06-17 02:51:35 +0300

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,45 +1,50 @@
 #include "lists.h"
 
 /**
- *insert_dnodeint_at_index - function that insert a node.
- *@h: the head of the list.
- *@idx: the index where to insert.
- *@n: the integer.
- *Return: new node.
+ * insert_dnodeint_at_index - function that insert a node.
+ *
+ * @h: the head of the list.
+ * @idx: the index where to insert.
+ * @n: the integer.
+ *
+ * Return: new node.
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-unsigned int i = 1;
-dlistint_t *tmp, *prevn;
-dlistint_t *new_node = malloc(sizeof(dlistint_t));
-if (*h == NULL || !new_node)
-return (NULL);
-new_node->n = n;
-tmp = (*h)->next;
-prevn = *h;
-while (tmp)
-if (idx == 0)
-{
-new_node->next = *h;
-(*h)->prev = new_node;
-return (new_node);
-}
-else
-{
-while (tmp)
-{
-if (idx == i)
-{
-new_node->next = tmp;
-tmp->prev = new_node;
-prevn->next = new_node;
-return (new_node);
-}
-prevn = tmp;
-tmp = tmp->next;
-i++;
-}
-}
-return (NULL);
+	dlistint_t *prevn, *temp;
+	unsigned int j = 1;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	
+	if (*h == NULL || !new_node)
+		return (NULL);
+	
+	new_node->n = n;
+	temp = (*h)->next;
+	prevn = *h;
+	
+	while (temp)
+		if (idx == 0)
+		{
+			new_node->next = *h;
+			(*h)->prev = new_node;
+			return (new_node);
+		}
+		else
+		{
+			while (temp)
+			{
+				if (idx == j)
+				{
+					new_node->next = temp;
+					temp->prev = new_node;
+					prevn->next = new_node;
+					return (new_node);
+				}
+				prevn = temp;
+				temp = temp->next;
+				j++;
+			}
+		}
+	return (NULL);
 }
