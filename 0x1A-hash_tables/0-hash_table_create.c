0f357ddd6b1df0bc2f077d5e9f51c4050c6512cd
AropetJ <aropetjoel32@gmail.com>
2023-07-07 21:27:44 +0300

    Edited the header file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,24 +12,23 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int j;
-	hash_table_t *h;
+	hash_table_t *hashTable;
 
-	h = malloc(sizeof(hash_table_t));
-
-	if (h == NULL)
+	hashTable = malloc(sizeof(hash_table_t));
+	if (hashTable == NULL)
 	{
 		return (NULL);
 	}
 
-	h->size = size;
-	h->array = malloc(sizeof(hash_node_t *) * size);
-	if (h->array == NULL)
+	hashTable->size = size;
+	hashTable->array = malloc(sizeof(hash_node_t *) * size);
+	if (hashTable->array == NULL)
 	{
 		return (NULL);
 	}
 	for (j = 0; j < size; j++)
 	{
-		h->array[j] = NULL;
+		hashTable->array[j] = NULL;
 	}
-	return (h);
+	return (hashTable);
 }
